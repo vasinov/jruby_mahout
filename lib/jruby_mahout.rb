09530dc8df97894c5184652025d1cae8e8aa0b09
Vasily Vasinov <des.elyon@gmail.com>
2012-11-18 21:30:42 -0700

    Setting up mahout.

--- lib/jruby_mahout.rb
+++ lib/jruby_mahout.rb
@@ -1,9 +1,18 @@
+require 'java'
+require File.join(MAHOUT_DIR, 'mahout-core-0.7.jar')
+require File.join(MAHOUT_DIR, 'mahout-integration-0.7.jar')
+require File.join(MAHOUT_DIR, 'mahout-math-0.7.jar')
+Dir.glob(File.join(MAHOUT_DIR, 'lib/*.jar')).each { |d| require d }
+
 require 'jruby_mahout/database_manager'
+require 'jruby_mahout/recommender_builder'
 
 module JrubyMahout
   def self.test
     database_manager = DatabaseManager.new("postgres")
     database_manager.load_database
     puts "machine learning is testing"
+
+    builder = RecommenderBuilder.new("PearsonCorrelationSimilarity", 4, "GenericUserBasedRecommender", false)
   end
 end
