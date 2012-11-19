fa0dadec1d934b83d93cc286ab3218622e9ba3e6
Vasily Vasinov <des.elyon@gmail.com>
2012-11-18 17:33:21 -0700

    Adding recommender code.

--- lib/jruby_mahout/recommender_builder.rb
+++ lib/jruby_mahout/recommender_builder.rb
@@ -1,5 +1,66 @@
 module JrubyMahout
   class RecommenderBuilder
+    # public interface RecommenderBuilder
+    # Implementations of this inner interface are simple helper classes which create a Recommender to be evaluated based on the given DataModel.
+    include org.apache.mahout.cf.taste.eval.RecommenderBuilder
 
+    def initialize(similarity_name, neighborhood_size, recommender_name, is_weighted)
+      @mahout_similarity = org.apache.mahout.cf.taste.impl.similarity
+      @mahout_neighborhood = org.apache.mahout.cf.taste.impl.neighborhood
+      @mahout_recommender = org.apache.mahout.cf.taste.impl.recommender
+      @mahout_recommender_slopeone = org.apache.mahout.cf.taste.impl.recommender.slopeone
+      @weighting = org.apache.mahout.cf.taste.common.Weighting
+      @is_weighted = is_weighted
+      @neighborhood_size = neighborhood_size
+      @similarity_name = similarity_name
+      @recommender_name = recommender_name
+    end
+
+    # buildRecommender(DataModel dataModel)
+    # Builds a Recommender implementation to be evaluated, using the given DataModel.
+    def buildRecommender(data_model)
+      begin
+        if @similarity_name == "PearsonCorrelationSimilarity"
+          if @is_weighted
+            similarity = @mahout_similarity.PearsonCorrelationSimilarity.new(data_model, @weighting::WEIGHTED)
+          else
+            similarity = @mahout_similarity.PearsonCorrelationSimilarity.new(data_model)
+          end
+        elsif @similarity_name == "EuclideanDistanceSimilarity"
+          if @is_weighted
+            similarity = @mahout_similarity.EuclideanDistanceSimilarity.new(data_model, @weighting::WEIGHTED)
+          else
+            similarity = @mahout_similarity.EuclideanDistanceSimilarity.new(data_model)
+          end
+        elsif @similarity_name == "SpearmanCorrelationSimilarity"
+          similarity = @mahout_similarity.SpearmanCorrelationSimilarity.new(data_model)
+        elsif @similarity_name == "LogLikelihoodSimilarity"
+          similarity = @mahout_similarity.LogLikelihoodSimilarity.new(data_model)
+        elsif @similarity_name == "TanimotoCoefficientSimilarity"
+          similarity = @mahout_similarity.TanimotoCoefficientSimilarity.new(data_model)
+        elsif @similarity_name == "GenericItemSimilarity"
+          # similarity = @mahout_similarity.GenericItemSimilarity.new(@mahout_similarity.PearsonCorrelationSimilarity.new(data_model), data_model)
+          similarity = @mahout_similarity.PearsonCorrelationSimilarity.new(data_model, @weighting::WEIGHTED)
+        end
+
+        if @neighborhood_size and (["1", "2", "4", "8", "16", "32", "64", "128"].include? @neighborhood_size) and similarity
+          neighborhood = @mahout_neighborhood.NearestNUserNeighborhood.new(Integer(@neighborhood_size), similarity, data_model)
+        end
+
+        if @recommender_name == "GenericUserBasedRecommender" and neighborhood
+          recommender = @mahout_recommender.GenericUserBasedRecommender.new(data_model, neighborhood, similarity)
+        elsif @recommender_name == "GenericItemBasedRecommender"
+          recommender = @mahout_recommender.GenericItemBasedRecommender.new(data_model, similarity)
+        elsif @recommender_name == "SlopeOneRecommender"
+          recommender = @mahout_recommender_slopeone.SlopeOneRecommender.new(data_model)
+        else
+          recommender = nil
+        end
+
+        return recommender
+      rescue java.lang.IllegalArgumentException => e
+        return nil
+      end
+    end
   end
 end
