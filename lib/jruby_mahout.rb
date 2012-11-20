8f1f11197c27ad6615504fbd0281bd61908ed82a
Vasily Vasinov <des.elyon@gmail.com>
2012-11-19 20:17:16 -0700

    Working on the main jruby_mahout methods.

--- lib/jruby_mahout.rb
+++ lib/jruby_mahout.rb
@@ -6,6 +6,7 @@ Dir.glob(File.join(ENV["MAHOUT_DIR"], 'lib/*.jar')).each { |d| require d }
 
 require 'jruby_mahout/database_manager'
 require 'jruby_mahout/recommender_builder'
+require 'jruby_mahout/data_model_builder'
 
 module JrubyMahout
   def self.test
@@ -14,12 +15,15 @@ module JrubyMahout
     puts "machine learning is testing"
   end
 
-  def self.create_recommender(similarity_name, neighborhood_size, recommender_name, is_weighted)
+  def self.initialize_recommender(similarity_name, neighborhood_size, recommender_name, is_weighted)
     recommender_builder = RecommenderBuilder.new(similarity_name,
                                                  neighborhood_size,
                                                  recommender_name,
                                                  is_weighted)
-    data_model = DatamodelBuilder.new("")
+  end
+
+  def self.build_recommender(recommender_builder, file_path)
+    data_model = DatamodelBuilder.new(file_path)
     recommender_builder.buildRecommender(data_model)
   end
 end
--- spec/recommender_builder_spec.rb
+++ spec/recommender_builder_spec.rb
@@ -3,8 +3,10 @@ require 'spec_helper'
 describe JrubyMahout do
   describe ".get_recommender_builder" do
     it "should return JrubyMahout::RecommenderBuilder" do
-      JrubyMahout.create_recommender("PearsonCorrelationSimilarity", 4, "GenericUserBasedRecommender", false).should
+      JrubyMahout.initialize_recommender("PearsonCorrelationSimilarity", 4, "GenericUserBasedRecommender", false).should
         be_an_instance_of JrubyMahout::RecommenderBuilder
     end
+
+    it "should re"
   end
 end
